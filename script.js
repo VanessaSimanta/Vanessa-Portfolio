1a30c4ce4280dd8c5fc6cfa47c93f1bef2632990
VanessaSimanta <132068764+VanessaSimanta@users.noreply.github.com>
2025-10-05 23:30:05 +0700

    update

---
+++ script.js
@@ -0,0 +1,30 @@
+// Back to top function
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
+
+// Nav Active function
+let navBar = document.getElementsByClassName("nav-link");
+
+for (let i = 0; i < navBar.length; i++) {
+  navBar[i].addEventListener("click", function() {
+    let current = document.getElementsByClassName("active");
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+    this.className += " active";
+  });
+}
