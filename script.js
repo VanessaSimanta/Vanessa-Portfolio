fecf69c32f55446a39fc935fd0008398315ace2d
VanessaSimanta <132068764+VanessaSimanta@users.noreply.github.com>
2025-10-16 14:20:25 +0700

    update

--- script.js
+++ script.js
@@ -28,3 +28,16 @@ for (let i = 0; i < navBar.length; i++) {
     this.className += " active";
   });
 }
+
+// Smooth Scroll
+document.querySelectorAll('.nav-link').forEach(link => {
+  link.addEventListener('click', function(e) {
+    if (this.hash !== "") {
+      e.preventDefault();
+      const target = document.querySelector(this.hash);
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
